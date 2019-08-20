9880ee263e5076135e21cc32522191c396215244
hericlesferreira <hericles_ps@hotmail.com>
2019-08-20 14:11:28 -0300

    Inclusão de footer e alteração do layout

--- services/postsServices.js
+++ services/postsServices.js
@@ -3,14 +3,38 @@ var getPosts = function() {
     {
         title: 'Post 1',
         image: 'post1.jpg',
-        description: 'Meu primeiro post',
-        body: 'Meu primeiro post blablabla'
+        body: 'Meu primeiro post',
+        description: 'Meu primeiro post blablabla'
     },
     {
       title: 'Post 2',
       image: 'post2.jpg',
-      description: 'Meu segundo post',
-      body: 'Meu segundo post blablabla'
+      body: 'Meu segundo post',
+      description: 'Meu segundo post blablabla aasjfasdfk asjfasç ka jasçfja kj ak jaf açsklj akjj j açklsjfk asj asjfçaksj'
+    },
+    {
+      title: 'Post 3',
+      image: 'post1.jpg',
+      body: 'Meu primeiro post',
+      description: 'Meu terceiro post blablabla asdf kajskf kajçskl ajk jka jsjçj jakçsjfaksjfashfjashjfhas ajshfajh aljh ajshflahasjkdfhj asjhaljkfhajfhasfjahhl ashdflajh askfhaljkfh asjkfhasjfhasjfa'
+    },
+    {
+      title: 'Post 4',
+      image: 'post2.jpg',
+      body: 'Meu quarto post',
+      description: 'Meu quarto post asdfajfjasç aksjklçj aklçs blablabla'
+    },
+    {
+      title: 'Post 1',
+      image: 'post1.jpg',
+      body: 'Meu primeiro post',
+      description: 'Meu primeiro post asdfajfçaskljk kajçsdkfjaçj  blablabla'
+    },
+    {
+      title: 'Post 2',
+      image: 'post2.jpg',
+      body: 'Meu segundo post',
+      description: 'Meu segundo post blablabla'
     },
   ];
 
