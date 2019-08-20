c2a3afcc840752b81618ec5b5be57668fe42f5af
hericlesferreira <hericles_ps@hotmail.com>
2019-08-20 20:27:50 -0300

    Adiciona visualização de um único post

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,31 @@ var postsService = require('../services/postsServices');
 router.get('/', function(req, res, next) {
   var posts = postsService.getPosts();
 
-  res.render('index', { title: 'Brog', posts: posts });
+  res.render('index', { title: 'Aniblogando', posts: posts });
+});
+
+router.get('/posts', function(req, res, next) {
+  var posts = postsService.getPosts();
+
+  res.render('posts', {title: 'Aniblogando - Posts', posts: posts});
+  
+});
+
+router.get('/posts/:postId', function(req, res, next) {
+  var postId = req.params.postId;
+
+  var posts = postsService.getPosts();
+
+//exemplo com for  for(var i=0; i<posts.length; i++) {
+//exemplo com for    if(posts[i].id == postId) {
+//exemplo com for      var post = posts[i];
+//exemplo com for    }
+//exemplo com for  }
+
+  var post = posts.filter((post) => post.id == postId)[0];
+
+  res.render('post', {title: post.title, post: post });
+
 });
 
 module.exports = router;
--- services/postsServices.js
+++ services/postsServices.js
@@ -1,40 +1,25 @@
 var getPosts = function() {
   var posts = [
     {
-        title: 'Post 1',
-        image: 'post1.jpg',
-        body: 'Meu primeiro post',
-        description: 'Meu primeiro post blablabla'
-    },
-    {
-      title: 'Post 2',
-      image: 'post2.jpg',
-      body: 'Meu segundo post',
-      description: 'Meu segundo post blablabla aasjfasdfk asjfasç ka jasçfja kj ak jaf açsklj akjj j açklsjfk asj asjfçaksj'
-    },
-    {
-      title: 'Post 3',
-      image: 'post1.jpg',
-      body: 'Meu primeiro post',
-      description: 'Meu terceiro post blablabla asdf kajskf kajçskl ajk jka jsjçj jakçsjfaksjfashfjashjfhas ajshfajh aljh ajshflahasjkdfhj asjhaljkfhajfhasfjahhl ashdflajh askfhaljkfh asjkfhasjfhasjfa'
-    },
-    {
-      title: 'Post 4',
-      image: 'post2.jpg',
-      body: 'Meu quarto post',
-      description: 'Meu quarto post asdfajfjasç aksjklçj aklçs blablabla'
-    },
-    {
+      id: 1,
       title: 'Post 1',
       image: 'post1.jpg',
-      body: 'Meu primeiro post',
-      description: 'Meu primeiro post asdfajfçaskljk kajçsdkfjaçj  blablabla'
+      description: 'Meu primeiro post',
+      body: 'Meu primeiro  aksjdfklçasfjasçdfjk asjçfklaj sfkaj sfçajskfsakfajskfj asf jasçklfjaskdfj afj askdfjaskfj askdfj ksdafjçskdf j askdlfjas lçfj asfkj asklçdfj asklçfj askçfj dfjsda ç ajsdfçkasjfçkasj asdkfajsç jakf asjfklçasjfkçl blablabla'
     },
     {
+      id: 2,
       title: 'Post 2',
       image: 'post2.jpg',
-      body: 'Meu segundo post',
-      description: 'Meu segundo post blablabla'
+      description: 'Meu segundo post',
+      body: 'Meu segundo post blablabla aasjfasdfk asjfasç ka jasçfja kj ak jaf açsklj akjj j açklsjfk asj asjfçaksj'
+    },
+    {
+      id: 3,
+      title: 'Post 3',
+      image: 'post2.jpg',
+      description: 'Meu terceiro post',
+      body: 'Meu terceiro post blablabla aasjfasdfk asjfasç ka jasçfja kj ak jaf açsklj akjj j açklsjfk asj asjfçaksj'
     },
   ];
 
